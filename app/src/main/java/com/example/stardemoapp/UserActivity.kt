d7b26fc9184b974b219f5d436d90e125483e6816
EliottDo <thanhlongny@gmail.com>
2021-08-08 12:09:57 +0700

    update

--- app/src/main/java/com/example/stardemoapp/UserActivity.kt
+++ app/src/main/java/com/example/stardemoapp/UserActivity.kt
@@ -1,5 +1,6 @@
 package com.example.stardemoapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -24,6 +25,7 @@ class UserActivity : BaseFullActivity() {
     private val binding by viewBinding(ActivityUserBinding::inflate)
     private val viewModel: UserViewModel by lazy { getViewModel{ Injection.provideUserViewModel()}}
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
